6568bfbe3ec1cc7889590d62b826e84346661cb0
dylan.greene <dylan.greene@bb9abed2-0933-0410-ade7-e5b856161268>
2010-08-03 18:43:50 +0000

    seo jobvite front end
    git-svn-id: svn+ssh://svn.opower.com/opt/svnroot/prototypes/opowerjobs@37319 bb9abed2-0933-0410-ade7-e5b856161268

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var connect = require('connect'),
    express = require('express'),
+   request = require('request'),
+   nStore = require('nstore'),
+   jobStore = nStore('data/jobs.db'),
    eyes = require('eyes'),
    pages = require('./pages').pages;
+
 
     var app = express.createServer(/*connect.logger()*/);
 
@@ -13,19 +17,259 @@ var connect = require('connect'),
         app.set('reload layout', 1000);
     }); 
 
+    function load_from_jobvite(callback) {
+        var jobvite_url = 'http://www.jobvite.com/CompanyJobs/Xml.aspx?c=qgY9Vfw2';
 
-    pages.forEach(function(page){
-        app.get('/' + page.url, function(req, res){
-            if (page.url == 'dev') {
-                res.redirect('http://www.heyitsopower.com');
-            }
-            else {
-                res.render((page.file || page.url) + '.ejs', { locals: { pages: pages, eyes: eyes, active: page.url.search('/') != -1 ? page.url.substr(0, page.url.search('/')) : page.url} });
+        request({uri:jobvite_url}, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var xml2js = require('xml2js');
+                var x2js = new xml2js.Parser();
+                x2js.addListener('end', function(data) {
+
+                    jobStore.clear();
+
+                    for (var i in data.job) {
+                        var job = data.job[i];
+                        // Or insert with auto key
+                        job.url = create_job_url(job);
+                        job.search_string = create_search_string(job);
+
+                        jobStore.save(job.id, job, function (err) {
+                            if (err) { throw err; }
+                        });
+
+
+
+                    }
+
+                    callback && callback();
+
+                });
+
+                x2js.parseString(body);
             }
         });
 
-    });
 
-   app.listen(3000);
+    }
 
-   console.log('\nStarting OPOWER JOBS...\n');
+
+    function create_job_url(job) {
+        var location_hash = {'Arlington': 'DC Northern Virgina'},
+            location = job.location.replace(/,.*/, ''),
+            location_url = location_hash[location] || location;
+
+        location_url = location_url.toLocaleLowerCase().replace(/[^a-z]+/gi, '-');
+
+        var department = job.category;
+        var department_url = department.toLocaleLowerCase().replace(/[^a-z]+/gi, '-');
+
+        var title_url = job.title.toLocaleLowerCase().replace(/[^a-z]+/gi, '-');
+
+        var long_url = [ '/jobs', location_url, department_url, title_url, job.id ].join('/');
+
+        return {location: location_url, department: department_url, title: title_url, long_url: long_url };
+    }
+
+    function remove_html(string) {
+        return string.replace(/<[^>]*>/g, ' ')
+                .replace(/&nbsp;/g, ' ');
+    }
+
+
+    function create_search_string(job) {
+        return remove_html([job.title, job.location, job.url.location, job.url.department, job.category, job.description]
+                .join(' ')
+                .toLocaleLowerCase());
+    }
+
+
+    var sort_jobs_helper = function(a, b) {
+        if (a.title < b.title) return -1;
+        if (a.title > b.title) return 1;
+        return 0;
+    };
+
+    function format_results(data) {
+        var jobs = { },
+            locations = {},
+            total = 0;
+
+        data.sort(sort_jobs_helper);
+
+        data.forEach(function(job) {
+            total++;
+
+            var urls = create_job_url(job);
+
+
+            var location = urls.location;
+            locations[location] = locations[location] || {};
+
+
+            var category = urls.department;
+            locations[location][category] = locations[location][category] || [];
+            locations[location][category].push(job);
+
+
+            job.opower_url = 'http://www.opower.com/Careers/CurrentOpportunities.aspx?nl=1&jvi=' + job.id + ',Job&jvk=Job';
+            job.url = urls.long_url;
+
+        });
+
+        jobs.locations = locations;
+
+        console.log('Loaded ' + total + ' jobs.');
+        return jobs;
+    }
+
+
+    function load_jobs(callback) {
+
+        jobStore.all(function (doc) {
+                return true; // return all;
+            }, function(err, data) {
+            var jobs = format_results(data);
+
+
+
+
+
+
+            app.get('/jobs/search/:query/json', function(req, res) {
+
+                var query = req.params.query;
+
+                jobStore.all(function (doc) {
+                    if (!doc.search_string) {
+                        return false;
+                    }
+
+                    var search_array = query.toLowerCase().split(/[\s|\+]/);
+                    for (var i = 0, l = search_array.length; i < l; i++) {
+                        var search_for = search_array[i];
+                        if (doc.search_string.search(search_for) === -1) {
+                            return false;
+                        }
+                    }
+
+                    return true;
+
+                }, function(err, data) {
+
+                    data.sort(sort_jobs_helper);
+
+                    var total_results = Math.min(data.length, 5),
+                        results = [];
+                    
+                    for (var i =0; i < total_results; i++) {
+                        var d = data[i];
+                        results.push({title: d.title, location: d.location, department: d.url.department, url: d.url.long_url });
+                    }
+
+                    results.push({ count: data.length, url: '/jobs/search/' + query });
+
+                    res.send(JSON.stringify(results), { 'Content-Type': 'application/json' });
+                });
+
+
+            });
+
+
+            app.get('/jobs/search/:query', function(req, res) {
+
+                var query = remove_html(req.params.query).replace(/[^a-zA-Z]/g, ' ').replace(/\s\s/g, ' ') || false;
+
+                jobStore.all(function (doc) {
+                        if (!doc.search_string) {
+                            return false;
+                        }
+
+                        var search_array = query.toLowerCase().split(/[\s|\+]/);
+                        for (var i = 0, l = search_array.length; i < l; i++) {
+                            var search_for = search_array[i];
+                            if (doc.search_string.search(search_for) === -1) {
+                                return false;
+                            }
+                        }
+
+                        return true;
+
+                    }, function(err, data) {
+                    if (err) { throw err; }
+                    var search_results = format_results(data);
+                    res.render('jobs.ejs', { locals: { pages: pages, eyes: eyes, active: 'opportunities', jobs: search_results, query: query } });
+
+                });
+
+            });
+
+
+            app.get('/jobs', function(req, res) {
+                res.render('jobs.ejs', { locals: { pages: pages, eyes: eyes, active: 'opportunities', jobs: jobs, query: '' } });
+            });
+
+            app.get('/jobs/:location', function(req, res) {
+                var location = req.params.location;
+                var local_jobs = { locations: {} };
+                local_jobs.locations[location] = jobs.locations[location];
+                res.render('jobs.ejs', { locals: { pages: pages, eyes: eyes, active: 'opportunities', jobs: local_jobs, query: '' } });
+            });
+
+            app.get('/jobs/:location/:department', function(req, res) {
+                var location = req.params.location,
+                    department = req.params.department;
+
+                var dept_jobs = { locations: {} };
+
+                dept_jobs.locations[location] = {};
+                dept_jobs.locations[location][department] = jobs.locations[location][department];
+                res.render('jobs.ejs', { locals: { pages: pages, eyes: eyes, active: 'opportunities', jobs: dept_jobs, query: '' } });
+            });
+
+            app.get('/jobs/:location/:category/:long_id/:id', function(req, res) {
+                var job_id = req.params.id;
+
+                jobStore.get(job_id, function(err, data) {
+                    res.render('job.ejs', { locals: { pages: pages, eyes: eyes, active: 'opportunities', jobs: jobs, job: data, query: '' } });
+                });
+            });
+
+
+
+            callback && callback();
+
+        });
+    }
+
+
+
+    function load_pages(){
+
+        pages.forEach(function(page){
+            app.get('/' + page.url, function(req, res){
+                if (page.url == 'dev') {
+                    res.redirect('http://www.heyitsopower.com');
+                }
+                else {
+                    res.render((page.file || page.url) + '.ejs', { locals: { pages: pages, eyes: eyes, active: page.url.search('/') != -1 ? page.url.substr(0, page.url.search('/')) : page.url} });
+                }
+            });
+
+        });
+    }
+
+    function load_server(callback) {
+        app.listen(3000);
+        console.log('\nStarting OPOWER JOBS...\n');
+        callback && callback();
+    }
+
+    load_pages();
+
+    //load_from_jobvite(load_jobs(load_server));
+
+    load_jobs(load_server);
+
+
+
--- public/js/site.js
+++ public/js/site.js
@@ -4,4 +4,69 @@ $(document).ready(function(){
     $('.logo').bind('click', function(){
         document.location = '/';
     });
+
+    function fix_query(query) {
+        return (query || '').replace(/[^a-zA-Z]/g, ' ').trim().replace(/\s+/g, '-')
+    }
+
+    var $q = $('#q');
+
+    if ($q.length) {
+
+        $('.search').bind('submit', function() {
+
+            var search_for = fix_query($q.val());
+            document.location = '/jobs/search/' + search_for;
+
+            return false;
+        });
+
+
+        var cache = {};
+        $q.autocomplete({
+            minLength: 2,
+            source: function(request, response) {
+                if ( request.term in cache ) {
+                    response( cache[ request.term ] );
+                    return;
+                }
+
+                var query = fix_query(request.term);
+
+                $.ajax({
+                    url: '/jobs/search/' + query + '/json',
+                    dataType: "json",
+                    data: request,
+                    success: function( data ) {
+                        cache[ request.term ] = data;
+                        response( data );
+                    }
+                });
+            },
+            select: function(event, ui) {
+                console.log('done');
+                document.location = ui.item.url;
+            }
+        })
+        .bind('focus', function() {
+            if ($q.val() && $q.autocomplete("widget").not(":visible")) {
+                $q.autocomplete("search", $q.val());
+            }
+        })
+        .data( "autocomplete" )._renderItem = function( ul, item ) {
+            if (!item) { return; }
+
+            if ('count' in item) {
+                return $( "<li></li>" )
+                    .data( "item.autocomplete", item )
+                    .append( item.count ? '<a>' + 'All ' + item.count + ' matches.' + '</a>' : 'No matches')
+                    .appendTo( ul );
+            }
+
+            return $( "<li></li>" )
+                .data( "item.autocomplete", item )
+                .append( "<a>" + item.title + "<br>" + item.location + "</a>" )
+                .appendTo( ul );
+            };
+    }
 });
