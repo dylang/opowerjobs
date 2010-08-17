d7700b66cc341210c9ceda63cb7749e79a7460cc
dylan.greene <dylan.greene@bb9abed2-0933-0410-ade7-e5b856161268>
2010-08-17 18:59:09 +0000

    cache buster for g
    git-svn-id: svn+ssh://svn.opower.com/opt/svnroot/prototypes/opowerjobs@38582 bb9abed2-0933-0410-ade7-e5b856161268

--- app.js
+++ app.js
@@ -13,32 +13,42 @@ var log = require('./lib/util/log').from(__filename),
     jobs = require('./lib/jobs'),
 
     viewsDir = __dirname + '/views',
+    port = parseInt(process.env.PORT || 3000),
     app = express.createServer();
       
 
-app.configure(function(){
+function common() {
     app.set('views', viewsDir);
     app.use(assetManager(assets.config));
     app.use(connect.staticProvider(__dirname + '/public'));
-});
+}
 
-app.configure('development', function(){
-    app.use(connect.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
+function production(){
     //app.use(connect.logger());
     app.use(connect.conditionalGet());
     app.use(connect.gzip());
     app.use(connect.errorHandler());
     assets.compress();
-});
+}
 
+function development() {
+    app.use(connect.errorHandler({ dumpExceptions: true, showStack: true }));
+}
+
+app.configure(common);
+
+//hack for testing poduction settings 
+if (port != 3000) {
+    app.configure('production', production);
+} else {
+    app.configure('development', development);
+    app.configure('production', production);
+}
 app.use(content.createServer( {views: viewsDir, assets: assets }) );
 app.use(jobs.createServer( { views: viewsDir, jobvite_company_id: 'qgY9Vfw2', assets: assets }) );
 app.get('/:a?/:b?/:c?', function(req, res, next) { log('404'); log(req.url); res.send('404: ' + req.url + ' not found'); next(); });
 
-app.listen(parseInt(process.env.PORT || 3000), null);
+app.listen(port, null);
 log('Starting OPOWER JOBS...');
 
 
