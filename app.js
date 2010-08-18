a4f7d02993c745873c6d111e368c58bac523fbc9
dylan.greene <dylan.greene@bb9abed2-0933-0410-ade7-e5b856161268>
2010-08-18 18:35:38 +0000

    some error checking stuff
    git-svn-id: svn+ssh://svn.opower.com/opt/svnroot/prototypes/opowerjobs@38716 bb9abed2-0933-0410-ade7-e5b856161268

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var log = require('./lib/util/log').from(__filename),
     assetHandler = require('connect-assetmanager-handlers'),
     assets = require('./lib/assets'),
 
+    jsonToHTML = require('./lib/util/prettyJSON'),
+
     Content = require('./lib/content'),
     Jobs = require('./lib/jobs'),
 
@@ -21,9 +23,13 @@ function common() {
     Server.set('views', viewsDir);
     Server.use(assetManager(assets.config));
     Server.use(Connect.staticProvider(__dirname + '/public'));
+    Server.helpers({assets: assets, currentPageID: false, pages: []});
+    //Server.use(Server.router);
+
 }
 
 function production(){
+    log('starting in production mode');
     //app.use(Connect.logger());
     Server.use(Connect.conditionalGet());
     Server.use(Connect.gzip());
@@ -32,28 +38,47 @@ function production(){
 }
 
 function development() {
+    log('starting in development mode');
+    Server.use(Connect.gzip());
     Server.use(Connect.errorHandler({ dumpExceptions: true, showStack: true }));
 }
 
 Server.configure(common);
 
+Server.error(function(err, req, res, next){
+        res.render('error.ejs', { locals: { error: err, debug: jsonToHTML } });
+});
+
+Server.configure('development', production);
+
 //hack for testing poduction settings
 if (port != 3000) {
-    Server.configure('production', production);
+    Server.configure(production);
 } else {
-    Server.configure('development', development);
     Server.configure('production', production);
 }
+
 Server.use(Content.createServer( {views: viewsDir, assets: assets }) );
 Server.use(Jobs.createServer( { views: viewsDir, jobvite_company_id: 'qgY9Vfw2', assets: assets }) );
 
 // Required for 404's to return something
-Server.get('/*', function(req, res, next) { log('404'); log(req.url); res.send('404: ' + req.url + ' not found'); next(); });
+Server.get('/*', function(req, res){
+    log('404: ' + req.url);
+    res.render('error.ejs', { locals: { error: "404 man, 404.", debug: function(x){ return x;} } });
+    throw new NotFound;
+});
+
+function NotFound(msg){
+    this.name = 'NotFound';
+    Error.call(this, msg);
+    Error.captureStackTrace(this, arguments.callee);
+}
+
 
 // For spark, an app launcher
 module.exports.server = Server;
 
 Server.listen(port, null);
-log('Starting OPOWER JOBS...');
+log('Starting OPOWER JOBS on ' + port + '...');
 
 
