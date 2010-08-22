e4b5ad3d89fb1cadb2d74fa63949896bd63b0a73
dylan.greene <dylan.greene@bb9abed2-0933-0410-ade7-e5b856161268>
2010-08-22 02:36:47 +0000

    simplified use of Server
    git-svn-id: svn+ssh://svn.opower.com/opt/svnroot/prototypes/opowerjobs@39055 bb9abed2-0933-0410-ade7-e5b856161268

--- app.js
+++ app.js
@@ -10,26 +10,32 @@ var log = require('./lib/util/log').from(__filename),
     assetHandler = require('connect-assetmanager-handlers'),
     assets = require('./lib/assets'),
 
-    jsonToHTML = require('./lib/util/prettyJSON'),
+    objToHTML = require('./lib/util/prettyJSON'),
 
     Content = require('./lib/content'),
     Jobs = require('./lib/jobs'),
 
     viewsDir = __dirname + '/views',
     port = parseInt(process.env.PORT || 3000),
+    public_host = 'opowerjobs.heroku.com',
     Server = Express.createServer();
 
 
 function common() {
     Server.set('views', viewsDir);
-    //Server.use(Connect.gzip());
-    //Server.use(Connect.favicon(__dirname + '/public/favicon.ico'));
+
+    Server.helpers({
+        debug: objToHTML,
+        log: log
+    });
+
+    Server.use(Connect.gzip());
+    Server.use(Connect.favicon(__dirname + '/public/favicon.ico'));
     
     Server.use(assetManager(assets.config));
     Server.use(Connect.staticProvider(__dirname + '/public'));
     Server.helpers({assets: assets, currentPageID: false, pages: []});
-    //Server.use(Server.router);
-
+    Server.use(Server.router);
 }
 
 function production(){
@@ -47,12 +53,6 @@ function development() {
 
 Server.configure(common);
 
-Server.error(function(err, req, res, next){
-        res.render('generic.ejs', { locals: { title: 'Error', message: jsonToHTML(err) } });
-        log('ERROR');
-        log(err);
-});
-
 Server.configure('development', development);
 
 //hack for testing poduction settings
@@ -61,31 +61,35 @@ if (port != 3000) {
 } else {
     Server.configure('production', production);
 }
-// Required for 404's to return something
 
-/*
-Server.get('/*', function(req, res, next){
-
-    var host = req.headers.host;
-    if (host != 'localhost' && host != 'www.opowerjobs.com') {
-        res.redirect('http://www.opowerjobs.com' + req.originalUrl);
-    }
-    next();
+Server.error(function(err, req, res, next){
+        res.render('generic.ejs', { locals: { title: 'Error', message: jsonToHTML(err) } });
+        log('ERROR');
+        log(err);
 });
-*/
+
+
+// Redirect other servers to the main one
+Server.get('/*', function(req, res, next){
+    var host = req.headers.host.split(':')[0];
+    if (host != 'localhost' && host != public_host) {
+        res.redirect('http://' + public_host + req.originalUrl);
+    } else {
+        next();
+    }
+});
 
 
 
+Content.createServer( {Server: Server });
+Jobs.createServer( { Server: Server});
 
-
-Server.use(Content.createServer( {views: viewsDir, assets: assets }) );
-Server.use(Jobs.createServer( { views: viewsDir, jobvite_company_id: 'qgY9Vfw2', assets: assets }) );
 
 
 // Required for 404's to return something
 Server.get('/*', function(req, res){
     log('404: ' + req.url);
-    res.render('generic.ejs', { locals: { message: "404 man, 404." } });
+    res.render('generic.ejs', { locals: { message: "404, man, 404." } });
 });
 
 
